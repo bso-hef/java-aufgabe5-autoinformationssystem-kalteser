2a370ed03e9a89c7764c92ab8519474d318a3601
nkandler0305 <kandlernico@gmail.com>
2025-03-06 12:44:02 +0100

    wersfdcx

--- src/main/java/de/bsohef/Main.java
+++ src/main/java/de/bsohef/Main.java
@@ -14,13 +14,15 @@ public class Main {
         System.out.println("Bitte geben sie an, wie viel sie pro 100 Kilometer verbrauchen: ");
         double verbrauch = eingabe.nextDouble();
 
-        System.out.println("Bitte geben sie an, wievile Kilometer sie fahren wollen: ");
+        System.out.println("Bitte geben sie an, wie viel Kilometer sie fahren wollen: ");
         double distanz = eingabe.nextDouble();
 
         System.out.println("Bitte geben sie den Preis pro Liter Benzin an: ");
         double preis = eingabe.nextDouble();
 
+        double resttank = tankinhalt - (verbrauch / 100 * distanz);
 
-
+        if(resttank < 0)
+            System.out.println("Achtung der Tank wird für dir Strecke nicht reichen");
     }
 }
