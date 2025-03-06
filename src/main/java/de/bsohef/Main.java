3a0d9b3dde82c03dbaafa5706423ca16f65e77fd
doberhoeller0306 <dominik.oberhoeller@gmail.com>
2025-03-06 12:34:08 +0100

    init

--- src/main/java/de/bsohef/Main.java
+++ src/main/java/de/bsohef/Main.java
@@ -8,6 +8,19 @@ public class Main {
 
         Scanner eingabe = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Bitte geben sie an, wie voll ihr Tank ist: ");
+        double tankinhalt = eingabe.nextDouble();
+
+        System.out.println("Bitte geben sie an, wie viel sie pro 100 Kilometer verbrauchen: ");
+        double verbrauch = eingabe.nextDouble();
+
+        System.out.println("Bitte geben sie an, wievile Kilometer sie fahren wollen: ");
+        double distanz = eingabe.nextDouble();
+
+        System.out.println("Bitte geben sie den Preis pro Liter Benzin an: ");
+        double preis = eingabe.nextDouble();
+
+
+
     }
 }
