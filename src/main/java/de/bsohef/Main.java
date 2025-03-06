084dc0a637407a269678fc8107bd32ea4164c5ef
doberhoeller0306 <dominik.oberhoeller@gmail.com>
2025-03-06 12:26:05 +0100

    init

--- src/main/java/de/bsohef/Main.java
+++ src/main/java/de/bsohef/Main.java
@@ -1,7 +1,13 @@
 package de.bsohef;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner eingabe = new Scanner(System.in);
+
+        System.out.println();
     }
 }
